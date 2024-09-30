d7a97fa1cc7ff85aa0dc1806f4699fa5fffbd7d6
cohengmc <34006245+cohengmc@users.noreply.github.com>
2024-09-30 11:20:12 -0400

    Added colors :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,49 @@
+import colors from './colors.json';
+
+function ColorObject({ colorInfo }) {
+    return (
+        <div
+            style={{
+                color: `#${colorInfo.hex}`,
+            }}
+        >
+            {colorInfo.name}
+        </div>
+    );
+}
+
+function ColorObjectWithComp({ colorInfo }) {
+    return (
+        <div style={{ display: 'flex', gap: '12px' }}>
+            <div style={{ width: '128px' }}>
+                <ColorObject colorInfo={colorInfo} />
+            </div>
+            <div>Complimentary: </div>
+            <div style={{ display: 'flex', gap: '12px' }}>
+                {colorInfo.comp.map(i => {
+                    return (
+                        <div key={i.hex}>
+                            <ColorObject colorInfo={i} />
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+
 const App = () => {
-    return <h1>Hello World</h1>;
+    return (
+        <div>
+            {colors.map(i => {
+                return (
+                    <div key={i.hex}>
+                        <ColorObjectWithComp colorInfo={i} />
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default App;
